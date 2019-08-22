de5fff01a28038f0cc97a2a5e74244b64241d30d
david <davlondonomon@unal.edu.co>
2019-08-22 02:15:50 -0500

    :(

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -275,6 +275,7 @@ function nuevoDuenno(id){
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var respuesta = this.responseText
+            console.log(respuesta)
             toggleMostrar("duenno-pop")
         }
     }
